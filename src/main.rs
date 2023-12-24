ae45e81c5d5e18cd16083ea84074ee28ee68db4b
Martin Arce <martin.arce.teixeira@gmail.com>
2023-12-24 22:55:15 +0100

    Update port

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,22 @@
 use std::net::UdpSocket;
 
 fn main() -> std::io::Result<()> {
-    println!("init server at 34254");
-    let socket = UdpSocket::bind("127.0.0.1:34254")?;
+    println!("init server at 2053");
+    let socket = UdpSocket::bind("127.0.0.1:2053")?;
     let mut buf = [0; 10];
 
     loop {
         match socket.recv_from(&mut buf) {
             Ok((size, source)) => {
-                println!("Ok Size{:?}, Source {:}, buf {:?}", size, source, buf);
-                let header = build_header(&[2; 10]);
+                println!(" Size {:?},", size);
+                println!("Source {:} ",  source);
+                println!("buf {:?}",  buf);
+                // let header = build_header(&[2; 10]);
+                let message = DnsMessage::new();
+                // let h = Header::new();
+
+                println!("Message {:?}", message);
+                println!("- - - - - - -  ");
                 let res = [];
                 socket
                     .send_to(&res, source)
@@ -20,28 +27,54 @@ fn main() -> std::io::Result<()> {
                 break Err(e);
             }
         }
-
     }
 }
 
-
-fn build_header(buff: &[u8; 10]) -> [u8; 10] {
-    println!("HEADER: {:?}", *buff);
-    *buff
+#[derive(Debug)]
+struct DnsMessage {
+    header: Header
+}
+impl DnsMessage {
+    fn new() -> Self {
+        Self {
+            header: Header::new()
+        }
+    }
 }
 
+#[derive(Debug)]
 struct Header {
-    id: u16
-    // QR: u16,
-    // OPCODE: u16,
-    // AA: u16,
-    // TC: u16,
-    // RD: u16,
-    // RA: u16,
-    // Z	Reserved: u16,
-    // RCODE: u16,
-    // QDCOUNT: u16,
-    // ANCOUNT: u16,
-    // NSCOUNT: u16,
-//     ARCOUNT: u16
+    id: u16,
+    qr: u16,
+    opcode: u16,
+    aa: u16,
+    tc: u16,
+    rd: u16,
+    ra: u16,
+    z	: u16,
+    rcode: u16,
+    qdcount: u16,
+    ancount: u16,
+    nscount: u16,
+    arcount: u16
+}
+
+impl Header {
+    fn new() -> Self {
+        Self {
+            id: 1234,
+            qr: 1,
+            opcode: 0,
+            aa:0,
+            tc:0,
+            rd:0,
+            ra:0,
+            z: 0,
+            rcode: 0,
+            qdcount: 0,
+            ancount:0,
+            nscount:0,
+            arcount:0 
+        }
+    }
 }
