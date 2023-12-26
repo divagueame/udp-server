94d13a511198c52928baca501584c8694847ce1a
Martin Arce <martin.arce.teixeira@gmail.com>
2023-12-26 23:21:04 +0100

    Refactor to header

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+
 use std::net::UdpSocket;
 
 fn main() -> std::io::Result<()> {
@@ -25,48 +26,41 @@ fn main() -> std::io::Result<()> {
 
 #[derive(Debug)]
 struct DnsMessage {
-    header: Header
+    header: Header,
+    question: Question
 }
 impl DnsMessage {
     fn new() -> Self {
         Self {
-            header: Header::new()
+            header: Header::new(),
+            question: Question::new()
         }
     }
-    fn encode(&self) -> [u8;12] {
-        let id = &self.header.id.to_be_bytes();
-        let qdcount = &self.header.qdcount.to_be_bytes();
-        let ancount = &self.header.ancount.to_be_bytes();
-        let nscount = &self.header.nscount.to_be_bytes();
-        let arcount = &self.header.arcount.to_be_bytes();
-        let mut bytes: [u8;12] = [0b0000_000; 12];
+    // fn encode(&self) -> [u8;12] {
+    fn encode(&self) -> Vec<u8> {
+        let mut encoded_message: Vec<u8> = vec![];
+        let header_bytes = self.header.encode();
+        encoded_message.extend(&header_bytes[..]);
 
-        bytes[0] = id[0]; 
-        bytes[1] = id[1];
+        encoded_message
+    }
 
-        let mut byte2: u8 = 0b0000_0000;
-        byte2 |= (self.header.qr as u8) << 7;
-        byte2 |= (self.header.opcode as u8) << 3;
-        byte2 |= (self.header.aa as u8) << 2;
-        byte2 |= (self.header.tc as u8) << 1;
-        byte2 |= (self.header.rd as u8) << 0;
-        bytes[2] = byte2;
+}
 
-        let mut byte3: u8 = 0b0000_0000;
-        byte3 |= (self.header.ra as u8) << 7;
-        byte3 |= (self.header.z) << 4;
-        byte3 |= (self.header.rcode) << 0;
-        bytes[3] = byte3;
+#[derive(Debug)]
+struct Question {
+    r#type: u16,
+    class: u16
 
-        bytes[4] = qdcount[0];
-        bytes[5] =  qdcount[1];
-        bytes[6] =  ancount[0];
-        bytes[7] =  ancount[1];
-        bytes[8] =  nscount[0];
-        bytes[9] = nscount[1];
-        bytes[10] = arcount[0];
-        bytes[11] = arcount[1];
-        bytes
+}
+
+impl Question {
+    fn new() -> Self {
+        Self {
+            r#type: 1,
+            class: 12
+        }
+
     }
 }
 
@@ -105,4 +99,40 @@ impl Header {
             arcount:0 
         }
     }
+    fn encode(&self) -> [u8;12] {
+        let id = &self.id.to_be_bytes();
+        let qdcount = &self.qdcount.to_be_bytes();
+        let ancount = &self.ancount.to_be_bytes();
+        let nscount = &self.nscount.to_be_bytes();
+        let arcount = &self.arcount.to_be_bytes();
+        let mut bytes: [u8;12] = [0b0000_000; 12];
+
+        bytes[0] = id[0]; 
+        bytes[1] = id[1];
+
+        let mut byte2: u8 = 0b0000_0000;
+        byte2 |= (self.qr as u8) << 7;
+        byte2 |= (self.opcode as u8) << 3;
+        byte2 |= (self.aa as u8) << 2;
+        byte2 |= (self.tc as u8) << 1;
+        byte2 |= (self.rd as u8) << 0;
+        bytes[2] = byte2;
+
+        let mut byte3: u8 = 0b0000_0000;
+        byte3 |= (self.ra as u8) << 7;
+        byte3 |= (self.z) << 4;
+        byte3 |= (self.rcode) << 0;
+        bytes[3] = byte3;
+
+        bytes[4] = qdcount[0];
+        bytes[5] =  qdcount[1];
+        bytes[6] =  ancount[0];
+        bytes[7] =  ancount[1];
+        bytes[8] =  nscount[0];
+        bytes[9] = nscount[1];
+        bytes[10] = arcount[0];
+        bytes[11] = arcount[1];
+        bytes
+    }
 }
+
